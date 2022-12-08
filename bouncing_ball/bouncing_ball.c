31f38eb1d3cae9dd44103a5b51e8597d06143dba
Daniel Cliche <dancliche@gmail.com>
2022-12-08 10:59:26 -0500

    Fix blitter code no longer compatible

--- bouncing_ball/bouncing_ball.c
+++ bouncing_ball/bouncing_ball.c
@@ -142,7 +142,7 @@ void wait_blit_done() {
 void init_draw_ball() {
     xreg_setw(BLIT_MOD_S, 0x0000);
     xreg_setw(BLIT_SRC_S, START_BOBS);
-    xreg_setw(BLIT_VAL_C, 0xFFFF);
+    xreg_setw(BLIT_XOR, 0x0000);
     xreg_setw(BLIT_MOD_D, WIDTH_B / 4 - 4);
     xreg_setw(BLIT_SHIFT, 0xFF00);
     xreg_setw(BLIT_LINES, 15);
@@ -150,8 +150,8 @@ void init_draw_ball() {
 
 void draw_ball(uint16_t x, uint16_t y, bool visible) {
     uint16_t shift = x & 0x3;
-    xreg_setw(BLIT_CTRL, 0x0002);
-    xreg_setw(BLIT_VAL_C, visible ? 0xFFFF : 0x0000);
+    xreg_setw(BLIT_CTRL, 0x0000);
+    xreg_setw(BLIT_ANDC, visible ? 0x0000 : 0xFFFF);
     xreg_setw(BLIT_MOD_S, shift > 0 ? 0xFFFF : 0x0000);
     xreg_setw(BLIT_MOD_D, WIDTH_B / 4 - 4 - (shift > 0 ? 1 : 0));
     xreg_setw(BLIT_DST_D, START_B + y * WIDTH_B / 4 + (x >> 2));
@@ -254,9 +254,10 @@ void xosera_demo()
     xreg_setw(PB_GFX_CTRL, 0x0055);
 
     // clear playfield B
-    xreg_setw(BLIT_CTRL, 0x0003);
+    xreg_setw(BLIT_CTRL, 0x0001);
     xreg_setw(BLIT_SRC_S, 0x0000);
-    xreg_setw(BLIT_VAL_C, 0xFFFF);
+    xreg_setw(BLIT_ANDC, 0x0000);
+    xreg_setw(BLIT_XOR, 0x0000);
     xreg_setw(BLIT_MOD_D, 0x0000);
     xreg_setw(BLIT_DST_D, START_B);
     xreg_setw(BLIT_SHIFT, 0xFF00);
